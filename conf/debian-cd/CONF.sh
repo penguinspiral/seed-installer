6c44ffe4416bb274c28a826f1d6ddce9acd7b480
Myles Grindon <penguinspiral@users.noreply.github.com>
2020-07-30 02:59:22 -0700

    [debian-cd] Specify release codename from Makefile
    The Makefile serves as the entrypoint from which the user specifies the
    Debian release: `DEBIAN_RELEASE`. This variable is passed into the
    'seed-installer' Docker container as an environment variable and finally
    referenced in CONF.sh.

--- conf/debian-cd/CONF.sh
+++ conf/debian-cd/CONF.sh
@@ -63,7 +63,7 @@ export BASEDIR=`pwd`
 # export CDNAME=debian
 
 # Building $codename cd set ...
-export CODENAME=buster
+export CODENAME=${DEBIAN_RELEASE}
 
 # By default use Debian installer packages from $CODENAME
 if [ -z "$DI_CODENAME" ]; then
